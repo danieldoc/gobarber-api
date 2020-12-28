f2ff34d6e982e29a8dd97f330b7c820c660f8732
Daniel <daniel.olicruz13@gmail.com>
2020-12-28 16:58:30 -0300

    Criado loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'Hello Mãe da foca in debug!' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Daniel',
+    email: 'daniel@curso.com',
+    password_hash: '123',
+  });
+
+  res.json(user);
 });
 
 export default routes;
